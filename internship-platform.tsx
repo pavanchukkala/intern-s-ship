fbb86cd9081fa5d828d99aace44e7dc3b774eca8
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-02-07 08:15:16 +0530

    Update internship-platform.tsx

--- internship-platform.tsx
+++ internship-platform.tsx
@@ -7,6 +7,7 @@ import { Filter, Search, Building, Globe, Users } from "lucide-react"
 const FILTERS = [
   "Paid",
   "Free",
+  "pavan gaadi rulu",
   "Stipend-based",
   "Hourly Pay",
   "Project-based",
