7744f0452e455499dc94d9ada6fd3aa40a438733
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-02-22 12:37:20 +0530

    Update firebase-internorgres.ts

--- lib/firebase-internorgres.ts
+++ lib/firebase-internorgres.ts
@@ -3,16 +3,23 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_INTERNOREGRES_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_INTERNOREGRES_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_INTERNOREGRES_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_INTERNOREGRES_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_INTERNOREGRES_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_INTERNOREGRES_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app =
-  getApps().length ? getApp("internorgres") : initializeApp(firebaseConfig, "internorgres");
-const db = getFirestore(app);
+let app;
+if (typeof window !== "undefined") {
+  // Only initialize on the client.
+  app =
+    getApps().find((a) => a.name === "internorgres") ||
+    initializeApp(firebaseConfig, "internorgres");
+}
+
+const db = app ? getFirestore(app) : null;
 
 export { db };
