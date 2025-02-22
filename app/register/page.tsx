1d6bae01beccba23c6ad4c3366df1ec75afb8154
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-02-22 12:29:29 +0530

    Update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { db } from "@/lib/firebase-internorgres";
 import { Globe, Sun, Moon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
