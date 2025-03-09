a50c12e48833a48eb9e5aab324aa76e3b1e46c48
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-03-09 21:54:51 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 import FilterPanel from "@/components/FilterPanel";
 import { db } from "@/lib/firebase-cardload";
 import useFilteredInternships from "@/hooks/useFilteredInternships";
-
+import { recommendInternships } from "@/lib/recommendation";
 // InternshipCard component
 function InternshipCard({ internship, activeCardId, setActiveCardId }: { 
   internship: any, 
