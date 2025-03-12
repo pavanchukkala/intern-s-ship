bf65cc87b50a8b7275086fe4d0762bf6ea480bfa
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-03-12 19:18:58 +0530

    Update ScrollProgress.tsx

--- components/ScrollProgress.tsx
+++ components/ScrollProgress.tsx
@@ -19,7 +19,7 @@ export default function ScrollProgress() {
   }, []);
 
   return (
-    <div className="fixed top-0 left-0 w-full h-2 z-50">
+    <div className="fixed top-0 left-0 w-full h-1.5 z-50">
   <div
     className="bg-green-600 h-full animate-pulse"
     style={{
