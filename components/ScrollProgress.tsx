a84aab6d9606bc3a22f4d3b22fb06f65977d745b
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-03-12 18:53:59 +0530

    Update ScrollProgress.tsx

--- components/ScrollProgress.tsx
+++ components/ScrollProgress.tsx
@@ -20,7 +20,7 @@ export default function ScrollProgress() {
 
   return (
     <div className="fixed top-0 left-0 w-full h-1 z-50">
-      <div className="bg-blue-600 h-full" style={{ width: `${scrollPercent}%` }}></div>
+      <div className="bg-green-600 h-full" style={{ width: `${scrollPercent}%` }}></div>
     </div>
   );
 }
