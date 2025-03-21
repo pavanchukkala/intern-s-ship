4c159bcade123654a406ad3c221dc7da1895d8c4
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-03-21 10:22:40 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 py-6 relative">
-      <div className="container mx-auto px-4 flex justify-center items-center">
+    <>
+      <footer className="bg-gray-200 dark:bg-gray-700 text-center py-6">
         <p className="text-sm">
           © {currentYear} Internsship. All rights reserved.
         </p>
-      </div>
-      <div className="absolute right-4 bottom-4">
+      </footer>
+      <div className="fixed bottom-4 right-4">
         <Link href="/legaldocs/privacy-policy">
-          <a className="text-sm hover:text-blue-500">Privacy Policy</a>
+          <a className="text-sm text-gray-600 dark:text-gray-300 hover:underline">
+            Privacy Policy
+          </a>
         </Link>
       </div>
-    </footer>
+    </>
   );
 }
