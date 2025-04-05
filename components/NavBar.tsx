f334c1a477d1b5bbe67af0836629bb941221dcc1
chukkala pavan kumar <150168684+pavanchukkala@users.noreply.github.com>
2025-04-05 14:09:37 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -25,7 +25,7 @@ export default function NavBar() {
        <Image
 src="/BasicAssets/Klogo.jpg"
   alt="Internship Icon"
-  width={75} // Set explicit width
+  width={85} // Set explicit width
   height={75} // Set explicit height
   className="rounded-full"
 />
